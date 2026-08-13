ad1105227ad76cdc4373483be55676933d5ab1fe
Sandhya Shakya <sandhyashakya300@gmail.com>
2026-08-13 11:05:22 +0530

    Add files via upload

---
+++ static.py
@@ -0,0 +1,15 @@
+class math:
+    def __init__(self,num):
+        self.num = num
+def addtonum(self, n):
+    self.num += n
+
+@staticmethod
+def add(x, y):
+    return x + y
+a = math(5)
+
+
+print(a.num)
+a.addtonum(10)
+print(a.num)
